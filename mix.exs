0ca0c3318617bc4ef38a1812822444b62ebbf244
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-03-28 19:21:22 -0700

    Installing hound for acceptance testing

--- mix.exs
+++ mix.exs
@@ -36,7 +36,8 @@ defmodule CreditCardChecker.Mixfile do
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:hound, "~> 0.8.2"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
