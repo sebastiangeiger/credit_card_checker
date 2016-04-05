add2fb5033b79b79e91d7ab0fcdf186419896a0c
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-04-05 16:47:18 -0700

    Redirect to expenses from root

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule CreditCardChecker.PageController do
   use CreditCardChecker.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    redirect conn, to: "/expenses"
   end
 end
