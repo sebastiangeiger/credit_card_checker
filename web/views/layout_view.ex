16eb4f40139048c9f8374c6dfeba6ad168d0872d
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-04-05 16:49:05 -0700

    Show nav pills to merchants/expenses/payment methods

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,14 @@
 defmodule CreditCardChecker.LayoutView do
   use CreditCardChecker.Web, :view
+  def nav_pill(name, opts) do
+    desired_path = Keyword.get(opts, :to)
+    current_path = Keyword.get(opts, :current_path)
+    inner = Phoenix.HTML.Link.link name, opts
+    li_opts = if (desired_path == current_path) do
+      "active"
+    else
+      ""
+    end
+    Phoenix.HTML.Tag.content_tag :li, inner, class: li_opts
+  end
 end
