a1fcdcabb19f4c893d752d64e010b950f7c82677
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-04-05 08:53:35 -0700

    Show dollar amount when editing expense

--- web/views/expense_view.ex
+++ web/views/expense_view.ex
@@ -2,7 +2,7 @@ defmodule CreditCardChecker.ExpenseView do
   use CreditCardChecker.Web, :view
 
   def in_dollars(amount) when is_integer(amount) do
-    "$#{Float.to_string(amount / 100, decimals: 2)}"
+    "#{Float.to_string(amount / 100, decimals: 2)}"
   end
 
   def in_dollars(_) do
