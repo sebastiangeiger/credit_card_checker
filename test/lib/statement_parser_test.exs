0c4f7ec59bfb51e8859881eb41cdf05becc12aba
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-04-20 17:20:29 -0700

    Convert the csv output to StatementLines

--- test/lib/statement_parser_test.exs
+++ test/lib/statement_parser_test.exs
@@ -2,9 +2,18 @@ defmodule CreditCardChecker.StatementParserTest do
   use ExUnit.Case
 
   alias CreditCardChecker.StatementParser
+  alias CreditCardChecker.StatementLine
 
   test "reading the csv file generates the correct StatementLines" do
     lines = StatementParser.parse("test/fixtures/format_1.csv")
     assert Enum.count(lines) == 4
+    line = %StatementLine{
+      amount_in_cents: -870,
+      address: "ADDRESS #1",
+      payee: "PAYEE #1",
+      reference_number: "24492156019719803515390",
+      posted_date: Ecto.Date.cast!({2013, 4, 21})
+    }
+    assert List.first(lines) == line
   end
 end
--- web/lib/statement_parser.ex
+++ web/lib/statement_parser.ex
@@ -1,8 +1,11 @@
 defmodule CreditCardChecker.StatementParser do
+  alias CreditCardChecker.StatementLine
+
   def parse(file) do
     file
     |> read
     |> split_heading_and_body
+    |> convert_to_maps
     |> convert_to_statement_lines
   end
 
@@ -15,11 +18,29 @@ defmodule CreditCardChecker.StatementParser do
     %{head: head, body: body}
   end
 
-  defp convert_to_statement_lines(%{head: head, body: body}) do
-    Enum.map(body, fn line -> convert_to_statement_line(head, line) end)
+  defp convert_to_maps(%{head: head, body: body}) do
+    Enum.map(body, fn line ->
+                     Enum.zip(head, line)
+                     |> Enum.into(%{})
+                   end)
   end
 
-  defp convert_to_statement_line(head, line) do
-    line
+  defp convert_to_statement_lines(maps) do
+    Enum.map(maps, &convert_to_statement_line/1)
+  end
+
+  defp convert_to_statement_line(map) do
+    %StatementLine{
+      address: map["Address"],
+      amount_in_cents: round(String.to_float(map["Amount"]) * 100),
+      payee: map["Payee"],
+      reference_number: map["Reference Number"],
+      posted_date: mm_dd_yyyy_to_date(map["Posted Date"])
+    }
+  end
+
+  defp mm_dd_yyyy_to_date(date_string) do
+    [_, m, d, y] = Regex.run(~r{(\d+)/(\d+)/(\d+)}, date_string)
+    Ecto.Date.cast!({y,m,d})
   end
 end
