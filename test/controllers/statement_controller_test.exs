6bf4b71c3c166ea7b3190e194540b44538d1cfa6
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-04-22 14:42:11 -0700

    Add tests for the statement controller

---
+++ test/controllers/statement_controller_test.exs
@@ -0,0 +1,40 @@
+defmodule CreditCardChecker.StatementControllerTest do
+  use CreditCardChecker.ConnCase
+
+  alias CreditCardChecker.StatementLine
+  import CreditCardChecker.AuthTestHelper, only: [sign_in: 2]
+
+  setup %{conn: conn} do
+    user = create_user(%{email: "somebody@example.com", password: "super_secret"})
+    payment_method = create_payment_method(%{name: "Visa"}, user: user)
+    payload = %{
+      conn: sign_in(conn, user),
+      user: user,
+      payment_method: payment_method,
+    }
+    {:ok, payload}
+  end
+
+  test "renders form for new resources", %{conn: conn, payment_method: payment_method} do
+    conn = get conn, payment_method_statement_path(conn, :new, payment_method)
+    assert html_response(conn, 200) =~ ~S{Upload statement for "Visa"}
+  end
+
+  test "create statements from a csv file", %{conn: conn, payment_method: payment_method} do
+    conn = post conn,
+                payment_method_statement_path(conn, :create, payment_method),
+                statement: %{file: %Plug.Upload{path: "test/fixtures/format_1.csv"}}
+
+    assert redirected_to(conn) == payment_method_path(conn, :show, payment_method)
+    assert Enum.count(Repo.all(StatementLine)) == 4
+    assert get_flash(conn, :info) == "Uploaded 4 statement lines"
+  end
+
+  test "Re-render if file is invalid", %{conn: conn, payment_method: payment_method} do
+    conn = post conn,
+                payment_method_statement_path(conn, :create, payment_method),
+                statement: %{}
+
+    assert html_response(conn, 200) =~ "No file given"
+  end
+end
--- web/controllers/statement_controller.ex
+++ web/controllers/statement_controller.ex
@@ -11,12 +11,20 @@ defmodule CreditCardChecker.StatementController do
 
   def create(conn, %{"payment_method_id" => payment_method_id} = params) do
     payment_method = Repo.get(PaymentMethod, payment_method_id)
-    result = params["statement"]["file"].path
-              |> StatementParser.parse
-              |> add_payment_method_id(payment_method_id)
-              |> Statement.insert_all
-    conn
-    |> redirect(to: payment_method_path(conn, :show, payment_method))
+    file = params["statement"]["file"]
+    if file do
+      {status, lines} = file.path
+                        |> StatementParser.parse
+                        |> add_payment_method_id(payment_method_id)
+                        |> Statement.insert_all
+      conn
+      |> put_flash(:info, "Uploaded #{Enum.count(lines)} statement lines")
+      |> redirect(to: payment_method_path(conn, :show, payment_method))
+    else
+      conn
+      |> put_flash(:error, "No file given")
+      |> render("new.html", payment_method: payment_method)
+    end
   end
 
   defp add_payment_method_id(statement_lines, payment_method_id) when is_integer(payment_method_id) do
