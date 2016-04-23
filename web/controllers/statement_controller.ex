fe456c1c678ae05680858c872b627dae0c52e81f
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-04-22 22:32:10 -0700

    Move parse & insert into Statement class
    This will either succeed or fail as a whole

--- web/controllers/statement_controller.ex
+++ web/controllers/statement_controller.ex
@@ -11,29 +11,16 @@ defmodule CreditCardChecker.StatementController do
 
   def create(conn, %{"payment_method_id" => payment_method_id} = params) do
     payment_method = Repo.get(PaymentMethod, payment_method_id)
-    file = params["statement"]["file"]
-    if file do
-      {status, lines} = file.path
-                        |> StatementParser.parse
-                        |> add_payment_method_id(payment_method_id)
-                        |> Statement.insert_all
-      conn
-      |> put_flash(:info, "Uploaded #{Enum.count(lines)} statement lines")
-      |> redirect(to: payment_method_path(conn, :show, payment_method))
-    else
-      conn
-      |> put_flash(:error, "No file given")
-      |> render("new.html", payment_method: payment_method)
+    upload = params["statement"]["file"]
+    case Statement.parse_and_insert(upload, payment_method_id) do
+      {:ok, lines} ->
+        conn
+        |> put_flash(:info, "Uploaded #{Enum.count(lines)} statement lines")
+        |> redirect(to: payment_method_path(conn, :show, payment_method))
+      _ ->
+        conn
+        |> put_flash(:error, "No file given")
+        |> render("new.html", payment_method: payment_method)
     end
-  end
-
-  defp add_payment_method_id(statement_lines, payment_method_id) when is_integer(payment_method_id) do
-    Enum.map(statement_lines, fn line ->
-      %{ line | payment_method_id: payment_method_id }
-    end)
-  end
-
-  defp add_payment_method_id({:ok, statement_lines}, payment_method_id) do
-    add_payment_method_id(statement_lines, String.to_integer(payment_method_id))
   end
 end
--- web/lib/statement.ex
+++ web/lib/statement.ex
@@ -13,4 +13,33 @@ defmodule CreditCardChecker.Statement do
       end
     end)
   end
+
+  def parse_file(statement_file) do
+    CreditCardChecker.StatementParser.parse(statement_file)
+  end
+
+  def parse_and_insert(nil, _payment_method_id)  do
+    {:error, "No file given"}
+  end
+
+  def parse_and_insert(%Plug.Upload{path: path}, payment_method_id)  do
+    parse_and_insert(path, payment_method_id)
+  end
+
+  def parse_and_insert(file, payment_method_id) when is_integer(payment_method_id) do
+    file
+    |> parse_file
+    |> add_payment_method_id(payment_method_id)
+    |> insert_all
+  end
+
+  def parse_and_insert(file, payment_method_id) do
+    parse_and_insert(file, String.to_integer(payment_method_id))
+  end
+
+  defp add_payment_method_id({:ok, statement_lines}, payment_method_id) when is_integer(payment_method_id) do
+    Enum.map(statement_lines, fn line ->
+      %{ line | payment_method_id: payment_method_id }
+    end)
+  end
 end
