7d10a9853e3e2a628d71db301dd68362db2e5c00
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-05-03 10:43:38 -0700

    Add helper method for creating statement lines

--- test/integration/create_a_transaction_test.exs
+++ test/integration/create_a_transaction_test.exs
@@ -2,6 +2,8 @@ defmodule CreditCardChecker.CreateATransactionTest do
   use CreditCardChecker.IntegrationCase
   import CreditCardChecker.ExpensesTestHelper,
     only: [create_expense: 1]
+  import CreditCardChecker.StatementsTestHelper,
+    only: [create_statement_line: 1]
   import CreditCardChecker.AuthTestHelper,
     only: [sign_in_through_app: 0]
 
@@ -11,13 +13,19 @@ defmodule CreditCardChecker.CreateATransactionTest do
   end
 
   test "can create a transaction" do
-    expenses = find_all_elements(:css, ".sem-expenses .sem-expense")
-    assert Enum.count(expenses) == 0
     create_expense %{amount: 3.11, merchant: %{name: "Whole Foods"},
                                    payment_method: %{name: "Amex"}}
-    navigate_to("/expenses")
-    expenses = find_all_elements(:css, ".sem-expenses .sem-expense")
-    assert Enum.count(expenses) == 1
+    navigate_to("/payment_methods")
+    find_element(:link_text, "Show")
+    |> click
+    statement_lines = find_all_elements(:css, ".sem-statement-lines .sem-statement-line")
+    assert Enum.count(statement_lines) == 0
+    create_statement_line %{amount: 3.11, payee: "WHOLE FDS", payment_method: %{name: "Amex"}}
+    navigate_to("/payment_methods")
+    find_element(:link_text, "Show")
+    |> click
+    statement_lines = find_all_elements(:css, ".sem-statement-lines .sem-statement-line")
+    assert Enum.count(statement_lines) == 1
   end
 end
 
