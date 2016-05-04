f30e08fa76e333decae9e2b18b934d672e3f5cb8
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-05-04 16:19:49 -0700

    Move common records into setup method

--- test/models/transaction_test.exs
+++ test/models/transaction_test.exs
@@ -4,12 +4,20 @@ defmodule CreditCardChecker.TransactionTest do
   import CreditCardChecker.Factory
   alias CreditCardChecker.Transaction
 
-  test "changeset with valid attributes" do
+  setup do
     user = create_user(%{email: "somebody@example.com", password: "super_secret"})
     payment_method = create_payment_method(%{name: "Visa"}, user: user)
-    statement_line = create_statement_line(%{amount: -12, payee: "Some Payee"},
+    records = %{
+      user: user,
+      payment_method: payment_method,
+      merchant: create_merchant(%{name: "Some Merchant"}, user: user),
+      statement_line: create_statement_line(%{amount: -12, payee: "Some Payee"},
                                              payment_method: payment_method)
-    merchant = create_merchant(%{name: "Some Merchant"}, user: user)
+    }
+    {:ok, records}
+  end
+
+  test "changeset with valid attributes", %{user: user, payment_method: payment_method, merchant: merchant, statement_line: statement_line} do
     expense = create_expense(%{amount: 12}, payment_method: payment_method,
                              merchant: merchant, user: user)
     transaction = Transaction.changeset(%Transaction{},
@@ -17,14 +25,9 @@ defmodule CreditCardChecker.TransactionTest do
     {:ok, _} = Repo.insert(transaction)
   end
 
-  test "create two transactions with the same expense" do
-    user = create_user(%{email: "somebody@example.com", password: "super_secret"})
-    payment_method = create_payment_method(%{name: "Visa"}, user: user)
-    sl_1 = create_statement_line(%{amount: -12, payee: "Some Payee"},
-                                             payment_method: payment_method)
+  test "create two transactions with the same expense", %{user: user, payment_method: payment_method, merchant: merchant, statement_line: sl_1} do
     sl_2 = create_statement_line(%{amount: -12, payee: "Some other Payee"},
                                              payment_method: payment_method)
-    merchant = create_merchant(%{name: "Some Merchant"}, user: user)
     expense = create_expense(%{amount: 12}, payment_method: payment_method,
                                merchant: merchant, user: user)
     transaction_1 = Transaction.changeset(%Transaction{},
@@ -36,12 +39,7 @@ defmodule CreditCardChecker.TransactionTest do
     assert changeset.errors[:expense_id] == "has already been taken"
   end
 
-  test "create two transactions with the same statement_line" do
-    user = create_user(%{email: "somebody@example.com", password: "super_secret"})
-    payment_method = create_payment_method(%{name: "Visa"}, user: user)
-    statement_line = create_statement_line(%{amount: -12, payee: "Some Payee"},
-                                             payment_method: payment_method)
-    merchant = create_merchant(%{name: "Some Merchant"}, user: user)
+  test "create two transactions with the same statement_line", %{user: user, payment_method: payment_method, merchant: merchant, statement_line: statement_line} do
     expense_1 = create_expense(%{amount: 12}, payment_method: payment_method,
                                merchant: merchant, user: user)
     expense_2 = create_expense(%{amount: 12}, payment_method: payment_method,
@@ -55,12 +53,7 @@ defmodule CreditCardChecker.TransactionTest do
     assert changeset.errors[:statement_line_id] == "has already been taken"
   end
 
-  test "create transaction with expense and statement not adding up" do
-    user = create_user(%{email: "somebody@example.com", password: "super_secret"})
-    payment_method = create_payment_method(%{name: "Visa"}, user: user)
-    statement_line = create_statement_line(%{amount: -12, payee: "Some Payee"},
-                                             payment_method: payment_method)
-    merchant = create_merchant(%{name: "Some Merchant"}, user: user)
+  test "create transaction with expense and statement not adding up", %{user: user, payment_method: payment_method, merchant: merchant, statement_line: statement_line} do
     expense = create_expense(%{amount: 13}, payment_method: payment_method,
                              merchant: merchant, user: user)
     transaction = Transaction.changeset(%Transaction{},
@@ -69,13 +62,8 @@ defmodule CreditCardChecker.TransactionTest do
     assert changeset.errors[:base] == "Amounts of StatementLine and Expense don't add up"
   end
 
-  test "create transaction with expense and statement from different payment methods" do
-    user = create_user(%{email: "somebody@example.com", password: "super_secret"})
-    payment_method_1 = create_payment_method(%{name: "Visa"}, user: user)
+  test "create transaction with expense and statement from different payment methods", %{user: user, payment_method: payment_method_1, merchant: merchant, statement_line: statement_line} do
     payment_method_2 = create_payment_method(%{name: "MasterCard"}, user: user)
-    statement_line = create_statement_line(%{amount: -12, payee: "Some Payee"},
-                                             payment_method: payment_method_1)
-    merchant = create_merchant(%{name: "Some Merchant"}, user: user)
     expense = create_expense(%{amount: 12}, payment_method: payment_method_2,
                              merchant: merchant, user: user)
     transaction = Transaction.changeset(%Transaction{},
