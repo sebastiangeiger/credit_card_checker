64daed16990bca7e4cbe2705a36af2f666a0ad25
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-05-17 18:37:10 -0700

    StatementLineExpenseMatcher can handle nil expense

--- test/services/statement_line_expense_matcher_test.exs
+++ test/services/statement_line_expense_matcher_test.exs
@@ -7,25 +7,27 @@ defmodule CreditCardChecker.StatementLineExpenseMatcherTest do
   alias CreditCardChecker.Merchant
   alias CreditCardChecker.PaymentMethod
 
-  test "view_model with a statement line and an expense returns the right datastructure" do
-    pm = %PaymentMethod{
+  @payment_method %PaymentMethod{
       name: "Visa"
     }
-    statement_line = %StatementLine{
-      amount_in_cents: -123,
-      posted_date: Ecto.Date.cast!("2015-04-20"),
-      payee: "MERCHANT #1",
-      address: "Address #1",
-      reference_number: "Reference Number #1",
-      payment_method: pm
-    }
+
+  @statement_line %StatementLine{
+    amount_in_cents: -123,
+    posted_date: Ecto.Date.cast!("2015-04-20"),
+    payee: "MERCHANT #1",
+    address: "Address #1",
+    reference_number: "Reference Number #1",
+    payment_method: @payment_method
+  }
+
+  test "view_model with a statement line and an expense returns the right datastructure" do
     expense = %Expense{
       amount_in_cents: 123,
       time_of_sale: Ecto.DateTime.cast!("2015-04-20 13:37:00"),
       merchant: %Merchant{name: "Merchant #1"},
-      payment_method: pm
+      payment_method: @payment_method
     }
-    view_model = StatementLineExpenseMatcher.view_model(statement_line, expense)
+    view_model = StatementLineExpenseMatcher.view_model(@statement_line, expense)
     assert view_model == [
       "Amount": ["-1.23", "1.23"],
       "Payee": ["MERCHANT #1", "Merchant #1"],
@@ -35,5 +37,26 @@ defmodule CreditCardChecker.StatementLineExpenseMatcherTest do
       "Payment Method": ["Visa", "Visa"]
     ]
   end
+
+  test "view_model with a statement line but no expense returns the right datastructure" do
+    view_model = StatementLineExpenseMatcher.view_model(@statement_line, nil)
+    assert view_model == [
+      "Amount": ["-1.23", ""],
+      "Payee": ["MERCHANT #1", ""],
+      "Date": ["2015-04-20", ""],
+      "Address": ["Address #1", ""],
+      "Reference Number": ["Reference Number #1", ""],
+      "Payment Method": ["Visa", ""]
+    ]
+  end
 end
 
+defmodule CreditCardChecker.StatementLineExpenseMatcher.ViewModelTest do
+  use ExUnit.Case
+
+  alias CreditCardChecker.StatementLineExpenseMatcher.ViewModel
+
+  test "zip_up with two keyword lists" do
+    assert ViewModel.zip_up([a: 1, b: 2], [a: 3, b: 4]) == [a: [1, 3], b: [2, 4]]
+  end
+end
--- web/services/statement_line_expense_matcher.ex
+++ web/services/statement_line_expense_matcher.ex
@@ -11,19 +11,53 @@ defmodule CreditCardChecker.StatementLineExpenseMatcher do
     [statement_line: statement_line, expenses: expenses]
   end
 
+  defmodule ViewModel do
+    defstruct lines: []
+
+    defp side(%StatementLine{} = statement_line) do
+      [
+        "Amount": in_dollars(statement_line.amount_in_cents),
+        "Payee": statement_line.payee,
+        "Date": Ecto.Date.to_string(statement_line.posted_date),
+        "Address": statement_line.address,
+        "Reference Number": statement_line.reference_number,
+        "Payment Method": statement_line.payment_method.name
+      ]
+    end
+
+    defp side(%Expense{} = expense) do
+      [
+        "Amount": in_dollars(expense.amount_in_cents),
+        "Payee": expense.merchant.name,
+        "Date": Ecto.DateTime.to_string(expense.time_of_sale),
+        "Address": "",
+        "Reference Number": "",
+        "Payment Method": expense.payment_method.name
+      ]
+    end
+
+    defp side(nil) do
+      [
+        "Amount": "",
+        "Payee": "",
+        "Date": "",
+        "Address": "",
+        "Reference Number": "",
+        "Payment Method": ""
+      ]
+    end
+
+    def zip_up(left_side, right_side) do
+      Keyword.merge(left_side, right_side,
+        fn(_key, value_1, value_2) -> [value_1, value_2] end)
+    end
+
+    def cast(statement_line, expense) do
+      zip_up(side(statement_line), side(expense))
+    end
+  end
+
   def view_model(statement_line, expense) do
-    ["Amount":
-      [in_dollars(statement_line.amount_in_cents),
-       in_dollars(expense.amount_in_cents)],
-     "Payee": [statement_line.payee, expense.merchant.name],
-     "Date":
-       [Ecto.Date.to_string(statement_line.posted_date),
-        Ecto.DateTime.to_string(expense.time_of_sale)],
-      "Address": [statement_line.address, ""],
-      "Reference Number": [statement_line.reference_number, ""],
-      "Payment Method":
-        [statement_line.payment_method.name,
-         expense.payment_method.name]
-    ]
+    ViewModel.cast(statement_line, expense)
   end
 end
