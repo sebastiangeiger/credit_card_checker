384a48c2277d0eea3b2684ed89b5a614d8e55a69
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-05-26 16:18:06 -0700

    Return unmatched_without_possible_expense from UnmatchedStatementLines

--- test/services/unmatched_statement_lines_test.exs
+++ test/services/unmatched_statement_lines_test.exs
@@ -5,6 +5,18 @@ defmodule CreditCardChecker.UnmatchedStatementLinesTest do
 
   import CreditCardChecker.Factory
 
+  test "view_model with a statement line and mis-matching expense" do
+    user = create_user(%{email: "somebody@example.com"})
+    merchant = create_merchant(%{name: "Whole Foods"}, user: user)
+    pm = create_payment_method(%{name: "Visa"}, user: user)
+    sl = create_statement_line(%{amount: -12.33, payee: "Whole Foods"}, payment_method: pm)
+    create_expense(%{amount: 12.34}, payment_method: pm, merchant: merchant, user: user)
+
+    assert UnmatchedStatementLines.view_model(user.id) ==
+      [unmatched_but_with_possible_expense: [],
+       unmatched_without_possible_expense: [sl]]
+  end
+
   test "view_model with a statement line and matching expense" do
     user = create_user(%{email: "somebody@example.com"})
     merchant = create_merchant(%{name: "Whole Foods"}, user: user)
@@ -13,6 +25,7 @@ defmodule CreditCardChecker.UnmatchedStatementLinesTest do
     create_expense(%{amount: 12.33}, payment_method: pm, merchant: merchant, user: user)
 
     assert UnmatchedStatementLines.view_model(user.id) ==
-      [unmatched_but_with_possible_expense: [sl]]
+      [unmatched_but_with_possible_expense: [sl],
+       unmatched_without_possible_expense: []]
   end
 end
--- web/services/unmatched_statement_lines.ex
+++ web/services/unmatched_statement_lines.ex
@@ -7,7 +7,21 @@ defmodule CreditCardChecker.UnmatchedStatementLines do
       [user_id: user_id]
       |> StatementLine.unmatched_but_with_possible_expense
       |> Repo.all
+    unmatched_expenses =
+      [user_id: user_id]
+      |> StatementLine.unmatched_debit_for_user
+      |> Repo.all
+    unmatched_without_possible_expense = diff(unmatched_expenses, unmatched_but_with_possible_expense)
+    [
+      unmatched_but_with_possible_expense: unmatched_but_with_possible_expense,
+      unmatched_without_possible_expense: unmatched_without_possible_expense
+    ]
+  end
 
-    [unmatched_but_with_possible_expense: unmatched_but_with_possible_expense]
+  defp diff(unmatched_expenses, unmatched_but_with_possible_expense) do
+    MapSet.difference(
+      MapSet.new(unmatched_expenses),
+      MapSet.new(unmatched_but_with_possible_expense))
+    |> MapSet.to_list
   end
 end
