e1c47ccf1bb8516816220e042cc26456d5a99a6e
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-05-26 08:54:49 -0700

    Converge test_helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,5 +2,4 @@ Application.ensure_all_started(:hound)
 
 ExUnit.start(timeout: 10_000)
 
-Ecto.Adapters.SQL.begin_test_transaction(CreditCardChecker.Repo)
-
+Ecto.Adapters.SQL.Sandbox.mode(CreditCardChecker.Repo, :manual)
