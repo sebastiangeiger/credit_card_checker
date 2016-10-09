e2b5ed94fe16fc66ec5c7414f6d22d190a980746
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-10-08 20:30:31 -0700

    Reimplement the ExpenseForm
      * This puts more logic into expense, not sure this is much better

--- web/models/expense.ex
+++ web/models/expense.ex
@@ -13,8 +13,8 @@ defmodule CreditCardChecker.Expense do
     timestamps
   end
 
-  @required_fields ~w(time_of_sale amount_in_cents merchant_id payment_method_id user_id)
-  @optional_fields ~w()
+  @required_fields ~w(time_of_sale amount_in_cents)
+  @optional_fields ~w(merchant_id payment_method_id user_id)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -22,15 +22,24 @@ defmodule CreditCardChecker.Expense do
   If no params are provided, an invalid changeset is returned
   with no validation performed.
   """
-  def changeset(model, params \\ %{}) do
+  def changeset(model, params, opts \\ []) do
     params = convert_amount(params)
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> add_assocs(opts)
     |> foreign_key_constraint(:user_id)
     |> foreign_key_constraint(:merchant_id)
     |> foreign_key_constraint(:payment_method_id)
     |> validate_merchant_belongs_to_user
     |> validate_payment_method_belongs_to_user
+  end
+
+  defp add_assocs(changeset, opts) do
+    if Keyword.has_key?(opts, :merchant) do
+      put_assoc(changeset, :merchant, opts[:merchant])
+    else
+      changeset
+    end
   end
 
   def mark_matched(expenses) when is_list(expenses) do
@@ -64,35 +73,76 @@ defmodule CreditCardChecker.Expense do
   end
 
   defp validate_payment_method_belongs_to_user(changeset) do
-    validate_belongs_to_same_user(changeset, :payment_method_id, CreditCardChecker.PaymentMethod)
-  end
-
-  defp validate_merchant_belongs_to_user(changeset) do
-    validate_belongs_to_same_user(changeset, :merchant_id, CreditCardChecker.Merchant)
-  end
-
-  defp validate_belongs_to_same_user(changeset, field, struct_type) do
     %{changes: changes, errors: errors} = changeset
 
     user_id = Map.get(changes, :user_id)
-    model_id = Map.get(changes, field)
+    model_id = Map.get(changes, :payment_method_id)
 
     model = if is_nil(model_id) do
       nil
     else
-      CreditCardChecker.Repo.get(struct_type, model_id)
+      CreditCardChecker.Repo.get(CreditCardChecker.PaymentMethod, model_id)
     end
 
     new  = if model && model.user_id == user_id do
       []
     else
-      [{field, "does not belong to the current user"}]
+      [{:payment_method_id, "does not belong to the current user"}]
     end
 
     case new do
       []    -> changeset
       [_|_] -> %{changeset | errors: new ++ errors, valid?: false}
     end
+  end
+
+  defp validate_merchant_belongs_to_user(changeset) do
+    %{changes: changes, errors: errors} = changeset
+
+    user_id = Map.get(changes, :user_id)
+
+    new = cond do
+      extract_merchant_user_id(changes) == user_id -> []
+      true -> [{:merchant_id, "does not belong to the current user"}]
+    end
+
+    case new do
+      []    -> changeset
+      [_|_] -> %{changeset | errors: new ++ errors, valid?: false}
+    end
+  end
+
+  defp extract_merchant_user_id(changes) do
+    get_in_changes(changes, [:merchant, :data, :user_id]) ||
+    get_in_changes(changes, [:merchant, :changes, :user_id]) ||
+    load_merchant_from_db(changes)
+  end
+
+  defp load_merchant_from_db(changes) do
+    merchant_id = Map.get(changes, :merchant_id)
+    merchant = if is_nil(merchant_id) do
+      nil
+    else
+      CreditCardChecker.Repo.get(CreditCardChecker.Merchant, merchant_id)
+    end
+    if merchant do
+      merchant.user_id
+    else
+      nil
+    end
+  end
+
+  # Somehow Kernel.get_in/2 won't work with a changeset
+  defp get_in_changes(nil, _path) do
+    nil
+  end
+
+  defp get_in_changes(result, []) do
+    result
+  end
+
+  defp get_in_changes(changeset, [ current | remainder ]) do
+    get_in_changes(Map.get(changeset, current), remainder)
   end
 
   def unmatched do
--- web/models/expense_form.ex
+++ web/models/expense_form.ex
@@ -12,14 +12,37 @@ defmodule CreditCardChecker.ExpenseForm do
   end
 
   def insert(params, user: user) do
-    #TODO: Values need to be shown properly if creation fails
-    Repo.transaction(fn ->
-      {merchant_params, expense_params} = Map.split(params, ["merchant_name"])
-      %{status: :ok, changeset: nil, merchant: nil}
-      |> create_or_find_merchant(merchant_params, user: user)
-      |> create_expense(expense_params, user: user)
-      |> rollback_if_necessary
-    end)
+    {merchant_params, expense_params} = Map.split(params, ["merchant_name"])
+    merchant_params = %{name: merchant_params["merchant_name"], user_id: user.id}
+    merchant = find_merchant(merchant_params)
+    expense_params = Map.put(expense_params, "user_id", user.id)
+    Expense.changeset(%Expense{}, expense_params, merchant: merchant)
+    |> Repo.insert
+    |> translate_errors
+  end
+
+  defp translate_errors({:error, %Ecto.Changeset{data: %Expense{}} = changeset}) do
+    {amount_error, errors} = Keyword.pop(changeset.errors, :amount_in_cents)
+    errors = case amount_error do
+      nil -> errors
+      _ -> Keyword.put_new(errors, :amount, amount_error)
+    end
+    merchant_errors = add_prefix_to_keys(changeset.changes.merchant.errors, :merchant_)
+    {:error, %{ changeset | errors: errors ++ merchant_errors, action: :insert }}
+  end
+
+  defp translate_errors({:ok, _} = result), do: result
+
+  defp find_merchant(merchant_params) do
+    get_merchant(merchant_params) || Merchant.changeset(%Merchant{}, merchant_params)
+  end
+
+  defp get_merchant(%{name: nil}) do
+    nil
+  end
+
+  defp get_merchant(merchant_params) do
+    Repo.get_by(Merchant, merchant_params)
   end
 
   def empty_changeset() do
@@ -33,71 +56,10 @@ defmodule CreditCardChecker.ExpenseForm do
     |> Timex.to_erlang_datetime
   end
 
-  defp create_or_find_merchant(result, merchant_params, user: user) do
-    merchant_params = %{name: merchant_params["merchant_name"], user_id: user.id}
-    case find_merchant(merchant_params) do
-      (%Merchant{} = merchant) -> %{ result | merchant: merchant }
-      nil ->
-        case create_merchant(merchant_params) do
-          {:ok, merchant} ->
-            %{ result | merchant: merchant }
-          {:error, changeset} ->
-            %{ result | status: :error, changeset: translate_changeset(changeset) }
-        end
-    end
-  end
-
-  defp find_merchant(%{name: nil}) do
-    nil
-  end
-
-  defp find_merchant(merchant_params) do
-    Repo.get_by(Merchant, merchant_params)
-  end
-
-  defp create_merchant(merchant_params) do
-    Merchant.changeset(%Merchant{}, merchant_params)
-    |> Repo.insert
-  end
-
-  defp create_expense(result, expense_params, user: user) do
-    case result do
-      %{status: :ok, merchant: merchant} ->
-        expense_params = Map.merge(expense_params,
-                                   %{"user_id" => user.id, "merchant_id" => merchant.id})
-        changeset = Expense.changeset(%Expense{}, expense_params)
-        case Repo.insert(changeset) do
-          {:ok, _} -> result
-          {:error, _} -> %{ result | status: :error, changeset: translate_changeset(changeset) }
-        end
-      %{status: :error} -> result
-    end
-  end
-
-  defp translate_changeset(%Ecto.Changeset{data: %Merchant{}} = changeset) do
-    {name_error, errors} = Keyword.pop(changeset.errors, :name)
-    errors = case name_error do
-      nil -> errors
-      _ -> Keyword.put_new(errors, :merchant_name, name_error)
-    end
-    time_of_sale = convert_time(Timex.DateTime.local)
-    %{ Ecto.Changeset.change(%__MODULE__{time_of_sale: time_of_sale}, %{}) | errors: errors, action: :insert }
-  end
-
-  defp translate_changeset(%Ecto.Changeset{data: %Expense{}} = changeset) do
-    {amount_error, errors} = Keyword.pop(changeset.errors, :amount_in_cents)
-    errors = case amount_error do
-      nil -> errors
-      _ -> Keyword.put_new(errors, :amount, amount_error)
-    end
-    %Ecto.Changeset{ errors: errors, action: :insert }
-  end
-
-  defp rollback_if_necessary(%{status: status} = result) do
-    case status do
-      :ok -> result[:changeset]
-      :error ->
-        Repo.rollback(result[:changeset])
-    end
+  defp add_prefix_to_keys(list, prefix) do
+    Enum.map(list, fn {key, value} ->
+      new_key = String.to_atom(Atom.to_string(prefix) <> Atom.to_string(key))
+      {new_key, value}
+    end)
   end
 end
