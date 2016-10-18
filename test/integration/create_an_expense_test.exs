4fed3a946750fd4a91002c1f083fec7d7772efa1
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-10-18 08:40:59 -0700

    Add ExpensesPage object, use it in tests

--- test/integration/create_an_expense_test.exs
+++ test/integration/create_an_expense_test.exs
@@ -4,29 +4,30 @@ defmodule CreditCardChecker.CreateAnExpenseTest do
   import CreditCardChecker.AuthTestHelper,
     only: [sign_in_through_app: 0]
 
+  alias ExpensesPage.Expenses
+
   setup do
     sign_in_through_app
     :ok
   end
 
   test "can create an expense" do
-    assert Enum.count(expenses_list) == 0
+    assert ExpensesPage.number_of_expenses == 0
     NewExpensePage.create(%{amount: 3.13,
       merchant: %{name: "Whole Foods"}, payment_method: %{name: "Amex"}})
-    alert_text = find_element(:css, ".alert")
-                  |> visible_text
-    assert alert_text =~ "Expense created successfully."
-    assert Enum.count(expenses_list) == 1
-    assert visible_page_text =~ "$3.13"
-    assert visible_page_text =~ "Whole Foods"
-    assert visible_page_text =~ "Amex"
+    assert ExpensesPage.alert =~ "Expense created successfully."
+    assert ExpensesPage.number_of_expenses == 1
+    expense = Expenses.get(0)
+    assert Expenses.merchant_name(expense) =~ "Whole Foods"
+    assert Expenses.amount(expense) =~ "$3.13"
+    assert Expenses.payment_method_name(expense) =~ "Amex"
   end
 
   test "can see payment methods in the new expense form" do
     NewPaymentMethodPage.create("Personal Visa")
     NewPaymentMethodPage.create("Golden Visa")
     NewPaymentMethodPage.create("Mastercard")
-    go_to_new_expense_form
+    NewExpensePage.visit
     options = find_all_elements(:css, "select#expense_payment_method_id option")
     |> Enum.map(&visible_text/1)
     assert options == ["Select payment method...", "Golden Visa", "Mastercard", "Personal Visa"]
@@ -35,7 +36,7 @@ defmodule CreditCardChecker.CreateAnExpenseTest do
   @tag failing_on_ci: true
   test "values are not lost when creating invalid expense" do
     NewPaymentMethodPage.create("Mastercard")
-    go_to_new_expense_form
+    NewExpensePage.visit
     find_element(:css, ".awesomplete input#expense_merchant_name")
     |> fill_field("Whole Foods")
     find_element(:css, "input#expense_amount")
@@ -50,16 +51,4 @@ defmodule CreditCardChecker.CreateAnExpenseTest do
              |> attribute_value("value")
     assert amount =~ "345.60"
   end
-
-  defp expenses_list do
-    navigate_to("/expenses")
-    find_all_elements(:css, ".sem-expenses .sem-expense")
-  end
-
-  defp go_to_new_expense_form do
-    navigate_to("/expenses")
-    find_element(:link_text, "+ New Expense")
-    |> click
-  end
 end
-
