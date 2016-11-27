7cace38c10f881b7e4832b98b035845922054512
Sebastian Geiger <sebastian.geiger@gmail.com>
2016-11-26 21:38:01 -0800

    Change url in code

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :credit_card_checker, CreditCardChecker.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "credit_card_checker.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "credit-card-checker.stian.in", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
